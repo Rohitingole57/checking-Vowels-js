cf0ebed286b98a259dd37352e860e514fa42eb20
Rohit Ingole <ingolerohit2017@gmail.com>
2023-02-14 11:39:49 +0530

    javascript vowels cheaking

---
+++ index.js
@@ -0,0 +1,18 @@
+var char = "a";
+
+if (char == "a") {
+    console.log("vowel");
+}
+else if (char = "e") {
+    console.log("vowel");
+}
+else if (char = "i") {
+    console.log("vowel");
+} else if (char = "o") {
+    console.log("vowel");
+}
+else if (char = "u") {
+    console.log("vowel");
+} else {
+    console.log("not a vowel");
+}
